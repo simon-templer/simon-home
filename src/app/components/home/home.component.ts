22695178114159ab7490a55f93539e56772e1b85
Simon Schmalz <simon.kai.schmalz@gmail.com>
2020-04-05 10:07:57 +0200

    home component added

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  @Input()
+  customImageUrl: string;
+  @Input()
+  title: string;
+  home = '../../../assets/img/Professional-resized.jpg';
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.customImageUrl)
+  }
+
+}
